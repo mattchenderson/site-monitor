667c17320008db231dda744788d43f3e820cf745
Matthew Henderson <matthew.henderson@microsoft.com>
2016-03-14 20:28:35 -0700

    Initializing project folders

---
+++ MobileApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MobileApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MobileApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobileApp.WebApiApplication" Language="C#" %>
